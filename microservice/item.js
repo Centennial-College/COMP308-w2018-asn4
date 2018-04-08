45e6f41be02afc55d5afce9ca82d92a8af25478e
vinoodpersad <vinood.persad@gmail.com>
2018-04-08 14:15:46 -0400

    Pasted all relevant code into both nodejs projects

--- microservice/item.js
+++ microservice/item.js
@@ -16,9 +16,18 @@ module.exports = function item(options) {
             respond(err, items.data$(false));
         });
     });
+    //define an action pattern to remove an item by name
+    seneca.add('role:item, cmd:remove', function remove(msg, respond) {
+        console.log('in remove action');
+        var items = seneca.make("items");
+        console.log('msg.id ' + msg.id);
+        items.remove$(msg.id, respond);
+    });
+
     //
     //define an action pattern to list the items
     seneca.add('role:item, cmd:fetch', function fetch(msg, respond) {
         var items = seneca.make("items");
         items.list$({}, respond);
     });
+}
